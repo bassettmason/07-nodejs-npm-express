3cdfb6f91edf9a6328454b962a7202172509b0be
Mason Bassett <bassettmason@gmail.com>
2018-04-18 11:00:42 -0700

    finished2

---
+++ mason-madeline/README.md
@@ -0,0 +1,20 @@
+# Blog with Server
+
+**Authors**: Mason Bassett, Madeline Peters
+**Version**: 1.0.0 
+
+## Overview
+We are building a blog application that has access to a server. This has implications for the future.
+
+## Getting Started
+The user must get the files from the repository and run them in their browser using port 3000 for now.
+
+## Architecture
+VS Code, HTML5, CSS3, jQuery, JS, Ajax, Handlebars.
+
+## Change Log
+04-18-2018 9am - Application now has a scaffolding.
+04-18-2018 10:20am -Application now has the server working and storing blog information.
+
+## Credits and Collaborations
+Thanks to  Noah Gribbin for explaining servers to us and answering questions. We also used Stack Overflow to troubleshoot error messages.
--- mason-madeline/server.js
+++ mason-madeline/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser').urlencoded({extended: true});
 let app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,13 +10,16 @@ app.use(express.static('./public'));
 
 
 app.get('/new', (request, response) => {
-  response.sendFile('index.html', {root:'./public'});
+  response.sendFile('new.html', {root:'./public'});
 })
-app.post('/articles', express.urlencoded(), function(request, response) {
+app.post('/articles', bodyParser, function(request, response) {
   response.status(201).json(request.body);
   console.log(request.body);
   response.send('Record posted to server!!');
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
+  
+
+})
 
 app.use((request, response) => {
   response.status(404).send('Sorry, that route does not exist.');
@@ -24,6 +28,3 @@ app.use((request, response) => {
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}!`);
 });
-  
-
-})

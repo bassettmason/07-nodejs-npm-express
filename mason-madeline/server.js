e4bcddf2eab30cbabc17c1fea17ff5046e1f8886
MADELINE PETERS <MADELINEPET@HOTMAIL.COM>
2018-04-18 10:12:21 -0700

    We have the server working and getting the post. Next we need to add to the readme and finish the responses.

--- mason-madeline/server.js
+++ mason-madeline/server.js
@@ -9,13 +9,19 @@ app.use(express.static('./public'));
 
 
 app.get('/new', (request, response) => {
-  response.sendFile('index.html', {root:'./public'});
-})
+  response.sendFile('new.html', {root:'./public'});
+});
 app.post('/articles', express.urlencoded(), function(request, response) {
   response.status(201).json(request.body);
   console.log(request.body);
+  console.log('response', response.body);
+
   response.send('Record posted to server!!');
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
+
+
+});
+
 
 app.use((request, response) => {
   response.status(404).send('Sorry, that route does not exist.');
@@ -24,6 +30,3 @@ app.use((request, response) => {
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}!`);
 });
-  
-
-})
